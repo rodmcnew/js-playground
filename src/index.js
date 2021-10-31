1db0359bdff8daa1fbd92fc84c5a1ad3fe0240e2
Rod Mcnew <rodmcnew@gmail.com>
2021-10-31 18:25:09 -0500

    add files

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const add = require("./add");
+
+let lastLastScore = 0;
+let lastScore = 1;
+let score = 1;
+while (score < 200) {
+    score = add(lastScore, lastLastScore);
+    console.log(`${lastScore} score and ${score} years ago...`);
+    lastLastScore = lastScore;
+    lastScore = score;
+}
